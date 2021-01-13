1217d9a506eedb7d046777c724501a009e5e0bfb
Nicoleta <nicoleta.platniuc@gmail.com>
2021-01-12 20:23:05 -0600

    First commit

---
+++ src/WarmUpChs/Counting_Valleys.java
@@ -0,0 +1,28 @@
+package WarmUpChs;
+
+public class Counting_Valleys {
+
+	public static void main(String[] args) {
+		System.out.println(countingValleys(12, "DDUUDDUDUUUD"));
+
+	}
+	
+	public static int countingValleys(int steps, String path) {
+	    // Write your code here
+	        int count = 0;
+	        int valeys = 0;
+	        
+	        for(int i=0; i < path.length(); i++){
+	            if(path.charAt(i)==('D')){
+	                count--;
+	            } else if(path.charAt(i)==('U')){
+	                count++;
+	            }
+	            if(count == 0 && path.charAt(i)==('U')){
+	                valeys++;
+	            }
+	        }
+	        return valeys;
+	    }
+
+	}
